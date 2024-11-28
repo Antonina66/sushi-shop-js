7f2d433e88a730ef1201080dd6c91651b889aed6
Antonina66 <88768298+Antonina66@users.noreply.github.com>
2024-11-28 21:26:00 +0300

    first commit

---
+++ constants/catalog.js
@@ -0,0 +1,60 @@
+const CATALOG = [
+  {
+    id: '1',
+    name: 'Филадельфия',
+    composition: 'Лосось, сливочный сыр, огурец.',
+    cost: '489',
+    img: 'https://images.sushishop.ru/assets/images/cache/docs/39/2025-filadelfiya.png/1727100014/1217x755.webp',
+    discount: '503',
+    grams: '245',
+    calories: '234',
+  },
+
+  {
+    id: '2',
+    name: 'Темпура с лососем',
+    composition:
+      'Лосось, снежный краб, сливочный сыр, огурец, соус Унаги, темпура.',
+    cost: '359',
+    img: 'https://images.sushishop.ru/assets/images/cache/docs/78063/2025-tempura-s-lososem.png/1727099316/1217x755.webp',
+    discount: '400',
+    grams: '205',
+    calories: '203',
+  },
+
+  {
+    id: '3',
+    name: 'Темпура с креветкой',
+    composition:
+      'Тигровая креветка, сливочный сыр, томат, омлет тамаго, соус Унаги, темпура, кунжут.',
+    cost: '359',
+    img: 'https://images.sushishop.ru/assets/images/cache/docs/78074/2025-tempura-s-krevetkoj.png/1727099287/1217x755.webp',
+    discount: '',
+    grams: '205',
+    calories: '219',
+  },
+
+  {
+    id: '4',
+    name: 'Запеченная Калифорния',
+    composition:
+      'Ролл Калифорния (снежный краб, сливочный сыр, огурец, икра масаго, соус унаги, кунжут) запеченный под 3-мя видами соусов: сырным, розовым, спайси и сыром моцарелла.',
+    cost: '333',
+    img: 'https://images.sushishop.ru/assets/images/cache/docs/49/2025-zapechennaya-kaliforniya.png/1727098500/1217x755.webp',
+    discount: '398',
+    grams: '250',
+    calories: '248',
+  },
+
+  {
+    id: '5',
+    name: 'Филадельфия с креветкой',
+    composition:
+      'Тигровая креветка, сливочный сыр, огурец, соус розовый, соус унаги, кунжут.',
+    cost: '389',
+    img: 'https://images.sushishop.ru/assets/images/cache/docs/38475/2025-filadelfiya-s-krevetkoj.png/1727099983/1217x755.webp',
+    discount: '',
+    grams: '220',
+    calories: '210',
+  },
+];
